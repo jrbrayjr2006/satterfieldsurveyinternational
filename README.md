e4b7d8f6d77505f568e03dc5a37d2b0a890ec8c3
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2017-09-03 18:16:32 -0500

    Created Home, Institutions, Patients, and Physicians components.  Also started added basic content to the Home component UI

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
 
+When building for a test or staging environment, use the `--base-href` switch if the deployment will be in a subdirectory.
+
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { InstitutionsComponent } from './institutions/institutions.component';
+import { PhysiciansComponent } from './physicians/physicians.component';
+import { PatientsComponent } from './patients/patients.component';
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'institutions', component: InstitutionsComponent},
+  { path: 'patients', component: PatientsComponent },
+  { path: 'physicians', component: PhysiciansComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InstitutionsComponent,
+    PhysiciansComponent,
+    PatientsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
