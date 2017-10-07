4de54847595bc535585e90cb0989becba17f49d6
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2017-10-07 15:57:36 -0500

    Added a new countries component and began fixing the sidenav toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { MdButtonModule, MdCheckboxModule, MatListModule, MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,12 +14,15 @@ import { PatientsComponent } from './patients/patients.component';
 
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { CountriesComponent } from './countries/countries.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'institutions', component: InstitutionsComponent},
   { path: 'patients', component: PatientsComponent },
-  { path: 'physicians', component: PhysiciansComponent }
+  { path: 'physicians', component: PhysiciansComponent },
+  { path: 'countries', component: CountriesComponent }
 ];
 
 @NgModule({
@@ -27,12 +32,19 @@ const appRoutes: Routes = [
     InstitutionsComponent,
     PhysiciansComponent,
     PatientsComponent,
-    HomeComponent
+    HomeComponent,
+    SidenavComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MatListModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+
+  @Output() sidenavToggle = new EventEmitter<boolean>();
+
+  sidenavOpen: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * An emitter is used to pass the event to the parent, but a service could be used as an alternative
+   */
+  openSideNav() {
+    console.info("Toggle the sidenav...");
+    if(this.sidenavOpen == false) {
+      console.info("Open sidenav...");
+      this.sidenavOpen = true;
+      this.sidenavToggle.emit(this.sidenavOpen);
+    } else {
+      console.info("Close sidenav...");
+      this.sidenavOpen = false;
+      this.sidenavToggle.emit(this.sidenavOpen);
+    }
+    
   }
 
 }
